49016c64cba4d22697fc4c982d912aa3192a7c30
Thomas Jalabert <thom.jalabert@gmail.com>
2017-03-31 16:12:00 +0200

    reduce tcf-test for CartCachierModule

--- j2e/tcf-test/src/test/java/arquillian/AbstractTCFTest.java
+++ j2e/tcf-test/src/test/java/arquillian/AbstractTCFTest.java
@@ -1,7 +1,6 @@
 package arquillian;
 
 import fr.unice.polytech.isa.tcf.CartModifier;
-import fr.unice.polytech.isa.tcf.asynchronous.KitchenPrinter;
 import fr.unice.polytech.isa.tcf.components.CartBean;
 import fr.unice.polytech.isa.tcf.components.carts.CartStatefulBean;
 import fr.unice.polytech.isa.tcf.entities.Customer;
@@ -27,22 +26,12 @@ public abstract class AbstractTCFTest {
 		// Building a Web ARchive (WAR) containing the following elements:
 		return ShrinkWrap.create(WebArchive.class)
 				.addAsWebInfResource(EmptyAsset.INSTANCE, "beans.xml")
-				// Message-Driven beans
-				.addPackage(KitchenPrinter.class.getPackage())
 				// Utils
 				.addPackage(Database.class.getPackage())
-				// Entities
-				.addPackage(Customer.class.getPackage())
-				// Components Interfaces
-				.addPackage(CartModifier.class.getPackage())
-				// Cart components
-				.addPackage(CartStatefulBean.class.getPackage())
-				// Interceptors
-				.addPackage(LogParameters.class.getPackage())
-				// Exceptions
-				.addPackage(AlreadyExistingCustomerException.class.getPackage())
 				// Components implementations
 				.addPackage(CartBean.class.getPackage())
+				// Cart components
+				.addPackage(CartStatefulBean.class.getPackage())
 				// Persistence file
 				.addAsManifestResource(new ClassLoaderAsset("META-INF/persistence.xml"), "persistence.xml");
 	}
